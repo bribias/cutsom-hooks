2744a83a8dc7cb07dcd69e12c2baf1d4892d699b
Brianna Bias <brianna.bias@gmail.com>
2021-09-24 20:26:16 -0700

    useDetailEvents added

--- src/hooks/useEvents.js
+++ src/hooks/useEvents.js
@@ -1,15 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchCharacters } from '../services/heyArnoldAPI';
+import { fetchCharacters, fetchOneCharacter } from '../services/heyArnoldAPI';
 
 export const useEvents = () => {
-    const [loading, setLoading] = useState(true);
-    const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
 
-    useEffect(() => {
-        fetchCharacters()
-            .then((characters) => setCharacters(characters))
-            .finally(() => setLoading(false));
-    }, []);
-    return { loading, characters };
+  useEffect(() => {
+    fetchCharacters()
+      .then((characters) => setCharacters(characters))
+      .finally(() => setLoading(false));
+  }, []);
+  return { loading, characters };
+};
+
+export const useDetailEvents = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    fetchOneCharacter(id)
+      .then((character) => setCharacter(character))
+      .finally(() => setLoading(false));
+  }, []);
+  return { loading, character };
 };
